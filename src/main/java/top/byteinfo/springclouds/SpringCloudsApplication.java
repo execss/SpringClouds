32cb6a2ae8f78b6093813391b790553ef8e7b045
byte <codingexec@outlook.com>
2022-04-15 17:28:30 +0800

    add

---
+++ src/main/java/top/byteinfo/springclouds/SpringCloudsApplication.java
@@ -0,0 +1,13 @@
+package top.byteinfo.springclouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/byteinfo/springclouds/SpringCloudsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.byteinfo.springclouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
